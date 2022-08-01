b2c63141f05733d3a763ef22e273bee098475ab0
cmskorcz <chaseskorey@icloud.com>
2022-08-01 18:18:42 -0400

    thought model added and exported

--- models/Thought.js
+++ models/Thought.js
@@ -3,7 +3,27 @@ const { Schema, model, Types } = require('mongoose');
 const ReactionSchema = new Schema(
     {
         reactionId: {
-            
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: new Date.now
+            // get:
+        }
+    },
+    {
+        toJSON: {
+            getters: true
         }
     }
 )
@@ -25,5 +45,15 @@ const ThoughtSchema = new Schema(
             required: true
         },
         reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
     }
-)
+);
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
