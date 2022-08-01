53ce569d60783dfcf150983cd33a6b1929389fe8
cmskorcz <chaseskorey@icloud.com>
2022-08-01 17:05:48 -0400

    added thought model schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,29 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
-const Thought = new Schema(
+const ReactionSchema = new Schema(
     {
-        
+        reactionId: {
+            
+        }
+    }
+)
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
     }
 )
