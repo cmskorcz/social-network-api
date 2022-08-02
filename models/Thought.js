3501873cc8a56b99a459b383cdaa0b5760b59272
cmskorcz <chaseskorey@icloud.com>
2022-08-02 11:01:44 -0400

    added format date util

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model, Types } = require('mongoose');
+const formatDate = require('../utils/format-date');
 
 const ReactionSchema = new Schema(
     {
@@ -17,8 +18,8 @@ const ReactionSchema = new Schema(
         },
         createdAt: {
             type: Date,
-            default: Date.now
-            // get:
+            default: Date.now,
+            get: createdAtVal => formatDate(createdAtVal)
         }
     },
     {
@@ -38,7 +39,8 @@ const ThoughtSchema = new Schema(
         },
         createdAt: {
             type: Date,
-            default: Date.now
+            default: Date.now,
+            get: createdAtVal => formatDate(createdAtVal)
         },
         username: {
             type: String,
